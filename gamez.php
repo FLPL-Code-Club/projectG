e0fa2decadbc359c6ce99d11004eedebd54cdc44
BlazeZexal <narutokid98@gmail.com>
2015-11-21 11:56:47 -0600

    Error feedback for testing

--- gamez.php
+++ gamez.php
@@ -2,7 +2,10 @@
  $query = "insert_into projectg.games (name, rating, thoughts) values '".$_POST["name"]."',".$_POST["rating"].",'".$_POST["thoughts"]."'";
 
  $connection = mysqli_connect("localhost", "projectg", "dumbledorespants2", "projectg");
- mysqli_query($connection, $query);
+ $result = mysqli_query($connection, $query);
 
+ if(!$result){
+ 	die("could not query, ya strange weirdo ".mysqli_error);
+ }
  header('Location: index.php');
  ?>
